a90056257c0211eb8170a625e9b7be6564427619
68110923@qq.com <44825280+68110923@users.noreply.github.com>
2020-07-28 22:36:46 +0800

    qwe

--- run.py
+++ run.py
@@ -2,4 +2,7 @@ from scrapy import cmdline
 # cmdline.execute('scrayp crawl qczjbmapp'.split())
 # cmdline.execute('scrayp crawl pornographic -s LOG_FILE=all.log'.split())
 # cmdline.execute('scrayp crawl DfVideoSpider'.split())
-cmdline.execute('scrayp crawl doubantop250'.split())
+# cmdline.execute('scrayp crawl doubantop250'.split())
+# cmdline.execute('scrayp crawl zhiyuan'.split())
+# cmdline.execute('scrayp crawl zhiyuan_biao'.split())
+cmdline.execute('scrayp crawl zhiyuan_gaodifen'.split())
